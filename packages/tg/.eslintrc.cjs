490e156f0a06e1a47d24328129c7531b5a674914
esoto76 <elpasocolorartist@yahoo.com>
2022-09-05 10:56:29 -0600

    feat(tg): add tailwindcss

--- packages/tg/.eslintrc.cjs
+++ packages/tg/.eslintrc.cjs
@@ -2,14 +2,23 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
+  root: true,
+  extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+    "@vue/eslint-config-prettier",
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest"
-  }
-}
+  env: {
+    node: true,
+    "vue/setup-compiler-macros": true,
+  },
+  rules: {
+    "vue/multi-word-component-names": [
+      "error",
+      {
+        ignores: [],
+      },
+    ],
+  },
+};
---
+++ packages/tg/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  presets: [require("@nhhsa/styles")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
