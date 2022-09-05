d0cabe7317e8bdd4f8d1e9789ff985b6a14239b1
esoto76 <elpasocolorartist@yahoo.com>
2022-09-05 11:02:48 -0600

    build(tg): add unplugin-auto-import

--- packages/tg/vite.config.ts
+++ packages/tg/vite.config.ts
@@ -1,15 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import AutoImport from 'unplugin-auto-import/vite';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'vue/macros', 'pinia'],
+      dts: './src/auto-imports.d.ts',
+      dirs: ['./src/stores/**'],
+      eslintrc: {
+        enabled: true,
+        filepath: './.eslintrc-auto-import.json',
+        globalsPropValue: true
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
