edd7fe9499e15b7c2591cc6d7a256d5f0d342041
esoto76 <elpasocolorartist@yahoo.com>
2022-09-19 12:08:19 -0600

    feat(tg): add icons

--- packages/tg/electron/main.ts
+++ packages/tg/electron/main.ts
@@ -1,39 +1,42 @@
-import { join } from 'path'
-import { app, BrowserWindow } from 'electron'
+import { join } from 'path';
+import { app, BrowserWindow } from 'electron';
 
-process.env.DIST = join(__dirname, '..')
-process.env.PUBLIC = app.isPackaged ? process.env.DIST : join(process.env.DIST, '../public')
+process.env.DIST = join(__dirname, '..');
+process.env.PUBLIC = app.isPackaged
+  ? process.env.DIST
+  : join(process.env.DIST, '../public');
 
-let win: BrowserWindow | null
+let win: BrowserWindow | null;
 // Here, you can also use other preload
-const preload = join(__dirname, './preload.js')
+const preload = join(__dirname, './preload.js');
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
-const url = process.env['VITE_DEV_SERVER_URL']
+const url = process.env['VITE_DEV_SERVER_URL'];
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: join(process.env.PUBLIC, 'logo.svg'),
+    icon: join(process.env.PUBLIC, 'icon.png'),
     webPreferences: {
       contextIsolation: false,
       nodeIntegration: true,
-      preload,
-    },
-  })
+      preload
+    }
+  });
+  win.setMenuBarVisibility(false);
 
   // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
+    win?.webContents.send('main-process-message', new Date().toLocaleString());
+  });
 
   if (app.isPackaged) {
-    win.loadFile(join(process.env.DIST, 'index.html'))
+    win.loadFile(join(process.env.DIST, 'index.html'));
   } else {
-    win.loadURL(url)
+    win.loadURL(url);
   }
 }
 
 app.on('window-all-closed', () => {
-  win = null
-})
+  win = null;
+});
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
