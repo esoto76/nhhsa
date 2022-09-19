52e365f58259964d43443f170f1c2fdb1617ffd7
esoto76 <elpasocolorartist@yahoo.com>
2022-09-19 13:29:01 -0600

    refactor(tg): updated Navbox

--- packages/tg/src/components/Navbox.vue
+++ packages/tg/src/components/Navbox.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="navbox">Navbox</div>
+  <div class="navbox">
+    <div class="navbar">
+      <div class="navbar-bx is-logo"><Logo /></div>
+      <div class="navbar-bx is-tile">
+        <h1>nhhsa</h1>
+        <h2>- transcipt generator</h2>
+      </div>
+      <div class="navbar-bx is-close">
+        <button class="navbar-btn">
+          <span class="material-icons">close</span>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
