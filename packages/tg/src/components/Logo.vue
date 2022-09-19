29d62ac0d3d83b797dfebd3dce4da3c6b4268ad4
esoto76 <elpasocolorartist@yahoo.com>
2022-09-19 13:25:15 -0600

    feat(tg): add Logo

---
+++ packages/tg/src/components/Logo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+<template>
+  <svg id="tg-icon" viewBox="0 0 384 384">
+    <rect class="icon-bg" fill="#27262c" width="384" height="384" />
+    <path
+      class="icon-letter"
+      fill="#f7e624"
+      d="M178.07,48c-6.35,9.08-10.07,20.11-10.07,32v16h-24v240h-48V96H48V48H178.07Z"
+    />
+    <path
+      class="icon-letter"
+      fill="#f7e624"
+      d="M288,144v-32c0-8.8-7.2-16-16-16h-16c-8.8,0-16,7.2-16,16v160c0,8.8,7.2,16,16,16h16c8.8,0,16-7.2,16-16v-32h-24v-48h72v112c0,17.6-14.4,32-32,32h-80c-17.6,0-32-14.4-32-32V80c0-17.6,14.4-32,32-32h80c17.6,0,32,14.4,32,32v64h-48Z"
+    />
+  </svg>
+</template>
